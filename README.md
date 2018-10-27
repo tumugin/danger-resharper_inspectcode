c0217b25b4b97700a44ea1de3ea3f4f95f691035
Kazuki Oishi <oishikazuki@gmail.com>
2018-10-28 01:10:02 +0900

    update README.md

--- README.md
+++ README.md
@@ -1,15 +1,30 @@
 # danger-resharper_inspectcode
 
-A description of danger-resharper_inspectcode.
+Danger plugin for JetBrains ReSharper InspectCode.
 
 ## Installation
 
-    $ gem install danger-resharper_inspectcode
+    PS> gem install danger-resharper_inspectcode
 
 ## Usage
+Before running Danger, you need to setup your CI service to run InspectCode Command-Line Tool to generate report XML file.
+```
+PS> /PATH/TO/TOOLS/InspectCode.exe YourSolution.sln; -o=report.xml
+```
 
-    Methods and attributes from this plugin are available in
-    your `Dangerfile` under the `resharper_inspectcode` namespace.
+Set your report file path on `Dangerfile` like this.
+```Ruby
+resharper_inspectcode.base_path = Dir.pwd
+resharper_inspectcode.report 'report.xml'
+```
+
+#### Attributes
+
+`base_path` - Base path of report file
+
+#### Methods
+
+`report` - Report warnings
 
 ## Development
 
--- lib/resharper_inspectcode/plugin.rb
+++ lib/resharper_inspectcode/plugin.rb
@@ -3,23 +3,12 @@
 require_relative "report_parser"
 
 module Danger
-  # This is your plugin class. Any attributes or methods you expose here will
-  # be available from within your Dangerfile.
-  #
-  # To be published on the Danger plugins site, you will need to have
-  # the public interface documented. Danger uses [YARD](http://yardoc.org/)
-  # for generating documentation from your plugin source, and you can verify
-  # by running `danger plugins lint` or `bundle exec rake spec`.
-  #
-  # You should replace these comments with a public description of your library.
-  #
-  # @example Ensure people are well warned about merging on Mondays
-  #
-  #          my_plugin.warn_on_mondays
-  #
-  # @see  tumugin/danger-resharper_inspectcode
-  # @tags monday, weekends, time, rattata
-  # s
+  # Danger plugin for JetBrains ReSharper InspectCode.
+  # @example Parse the report XML file and do reporting
+  #          resharper_inspectcode.base_path = Dir.pwd
+  #          resharper_inspectcode.report 'report.xml'
+  # @see tumugin/danger-resharper_inspectcode
+  # @tags C#, InspectCode, lint
   class DangerResharperInspectcode < Plugin
     # Base path of report file
     #
